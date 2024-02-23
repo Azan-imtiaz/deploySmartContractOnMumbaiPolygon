e806a57e3ade8cc4e9ed32a90d8fa4a7855a0174
Azan-imtiaz <azanimtiaz43@gmail.com>
2024-02-23 09:52:48 -0800

    first commit

---
+++ abi.js
@@ -0,0 +1,31 @@
+const abi= [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "num",
+          "type": "uint256"
+        }
+      ],
+      "name": "store",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "retrieve",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    }
+  ]
+  let deployedContractAddress="0x8865d0Bcc0284adb439B99a8C4A096e10a7E1Ec4";
+  module.exports={abi,deployedContractAddress};
